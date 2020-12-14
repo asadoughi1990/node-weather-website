f433e45468ec7fa922716db4006b7ac4d43f70d9
Ali Sadoughi <asadoughi1990@gmail.com>
2020-12-14 21:22:53 +0330

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=92c4125dbe53debdc252d8ede2eb4c2a&query=' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            const wData = body.current
+            callback(undefined, `${wData.weather_descriptions[0]}. It is currently ${wData.temperature} degress out. It feels like ${wData.feelslike} degress out`)
+        }
+    })
+}
+
+module.exports = forecast
